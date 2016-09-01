49ada3ddead28ed0ee451023abe70f4233cf8154
jc299562 <griffinbarath>
2016-09-01 10:41:25 +1000

    Updates quickPickGenerator.py in Prac04 based on feedback from tutor, adds unique number to quick pick and less complicated looping and list functions.

--- Prac04/quickPickGenerator.py
+++ Prac04/quickPickGenerator.py
@@ -5,16 +5,18 @@ def main():
     MIN_NUMBER = 1
     MAX_NUMBER = 45
     NUMBERS_IN_PICK = 6
-    counter = int(input("How many Quick Picks:"))
-    while counter < 1:
-        counter = input("Please enter a number greater than 0 to play!\nHow many Quick Picks?")
-    quickPicks = [1, 2, 3, 4, 5, 6, ]
-    for pick in range(counter):
-        for number in quickPicks:
-            for i in range(NUMBERS_IN_PICK):
-                quickPicks[i] = random.randint(MIN_NUMBER, MAX_NUMBER)
-            quickPicks.sort()
-            print(number, end=" ")
+    number_of_picks = int(input("How many Quick Picks:"))
+    while number_of_picks < 1:
+        number_of_picks = input("Please enter a number greater than 0 to play!\nHow many Quick Picks?")
+    for pick in range(number_of_picks):
+        quickPicks = []
+        for i in range(NUMBERS_IN_PICK):
+            randomNumber = random.randint(MIN_NUMBER, MAX_NUMBER)
+            while randomNumber in quickPicks:
+                randomNumber = random.randint(MIN_NUMBER, MAX_NUMBER)
+            quickPicks.append(randomNumber)
+        quickPicks.sort()
+        print(quickPicks, end=" ")
         print("")
 
 
