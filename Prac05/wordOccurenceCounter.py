50e2e000a6621b58f7d9c23eea060a86fea5b7ab
Griffin's <griffinbarath@hotmail.com>
2016-09-07 23:32:54 +1000

    Finishes Prac05, adds completed files wordOccurenceCounter and .gitignore file to Prac05.

---
+++ Prac05/wordOccurenceCounter.py
@@ -0,0 +1,23 @@
+def main():
+    WORD_OCCURENCES = {}
+    userSentence = str(input("Please enter a sentence containing at least 10 words:"))
+    userSentence = str.lower(userSentence)
+    seperatedSentence = str.split(userSentence)
+    seperatedSentence.sort()
+    for word in seperatedSentence:
+        if word not in WORD_OCCURENCES:
+            WORD_OCCURENCES[word]= 1
+        else:
+            WORD_OCCURENCES[word] += 1
+    longestWordLength = 0
+    for entry in WORD_OCCURENCES:
+        entryLength = len(entry)
+        if entryLength > longestWordLength:
+            longestWordLength = len(entry)
+    for entry in WORD_OCCURENCES:
+        print("{:{}} : {}".format(entry, longestWordLength, str(WORD_OCCURENCES[entry])))
+
+
+main()
+
+
