c87802a7880ea8162416d1a2fd34195e56b60295
griffinbarath <griffinbarath@hotmail.com>
2016-10-13 10:49:38 +1000

    Adds unfinished files to Prac10.

---
+++ Prac10/getFixedFilename.py
@@ -0,0 +1,12 @@
+import os, shutil
+
+__author__ = 'Griffin Barath'
+
+os.chdir('Lyrics\Christmas')
+
+for filename in os.listdir('.'):
+    newName = filename[0]
+    for char in filename:
+        if char.upper:
+
+    print(filename)
